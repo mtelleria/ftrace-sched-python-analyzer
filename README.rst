b8cf0d471b32adc06d2c45e6186dc55091838f0b
Miguel Telleria de Esteban <miguel@mtelleria.com>
2014-06-10 21:02:26 +0200

    First version of setup and README.rst files

---
+++ README.rst
@@ -0,0 +1,9 @@
+Python FTRACE trace-cmd fragment analyser
+=========================================
+
+This program parses the output of ::
+
+  trace-cmd report
+
+and computes execution groups to analyse time duration probability
+distributions and other statistics.
---
+++ setup.py
@@ -0,0 +1,22 @@
+"""Setup file for distribution"""
+from setuptools import setup
+
+with open("README.rst") as fh:
+    long_description = fh.read()
+
+with open("VERSION") as fh:
+    version = fh.read().strip()
+
+setup(
+    name = "ftrace_frag",
+    version = version,
+    license = "GPLv3",
+    description = "Program to parse FTRACE output and obtain execution fragments",
+    long_description = long_description,
+    py_modules=["timestamp"],
+    package_dir = {"":"src"},
+    classifiers = ["Development Status :: 3 - Alpha",
+                   "License :: OSI Approved :: GPLv3",
+                   "Programming Language :: Python",
+                  "Programming Language :: Python :: 2.7"]
+    )
